36719648f027d524f26c43696647b66d272e4650
Flopy <flortrochevilca24@gmail.com>
2026-07-30 21:27:34 -0300

    Arreglo refresco automático de notas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("Script cargado correctamente");
 const API_URL = "https://dju1980u4e.execute-api.us-east-1.amazonaws.com/notas";
 
 const boton = document.getElementById("guardar");
@@ -19,7 +20,27 @@ boton.addEventListener("click", async () => {
 
     if (respuesta.ok) {
         alert("✅ Nota guardada correctamente");
+        cargarNotas();
     } else {
         alert("❌ Error al guardar la nota");
     }
 });
+
+async function cargarNotas() {
+    const respuesta = await fetch(API_URL);
+    const notas = await respuesta.json();
+
+    console.log(notas);
+
+    const contenedor = document.getElementById("lista-notas");
+    contenedor.innerHTML = "";
+
+    notas.forEach(nota => {
+        const div = document.createElement("div");
+        div.innerHTML = `<h3>${nota.Titulo}</h3><p>${nota.Contenido}</p><hr>`;
+        contenedor.appendChild(div);
+        
+    });
+}
+
+cargarNotas();
